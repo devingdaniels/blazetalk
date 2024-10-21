1736c28654ea8a6580c34bd23e0516c6f222fc03
Devin Daniels <devingdaniels@gmail.com>
2024-10-21 09:54:37 +0530

    Fixing cors deployment bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,31 +5,23 @@ import { envs } from './common/envs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS for the frontend
-  // const allowedOrigins = [
-  //   'http://localhost:3010', // Local development URL
-  //   'https://realtalk-p1vm019ak-devingdaniels-projects.vercel.app', // Vercel deployment URL
-  //   'https://blazetalk-f7qvwe3m1-devingdaniels-projects.vercel.app/',
-  // ];
-
-  // app.enableCors({
-  //   origin: (origin, callback) => {
-  //     if (!origin || allowedOrigins.includes(origin)) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   credentials: true,
-  // });
-
+  // Enable CORS
   app.enableCors({
-    origin: true,
+    origin: (origin, callback) => {
+      if (!origin) {
+        callback(null, true);
+      } else {
+        callback(null, true);
+      }
+    },
     credentials: true,
+    methods: 'GET,HEAD,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
   });
 
-  await app.listen(envs.PORT);
+  await app.listen(envs.PORT || 3000);
 }
+
 bootstrap().then(() => {
   console.log('SUCCESSFULLY STARTED THE SERVER');
 });
