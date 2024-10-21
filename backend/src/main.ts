3bab35891df309627d961c32315820c7461b5d72
Devin Daniels <devingdaniels@gmail.com>
2024-10-21 09:56:31 +0530

    clean code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,23 +5,13 @@ import { envs } from './common/envs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin) {
-        callback(null, true);
-      } else {
-        callback(null, true);
-      }
-    },
+    origin: true,
     credentials: true,
-    methods: 'GET,HEAD,PUT,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
   });
 
-  await app.listen(envs.PORT || 3000);
+  await app.listen(envs.PORT);
 }
-
 bootstrap().then(() => {
   console.log('SUCCESSFULLY STARTED THE SERVER');
 });
