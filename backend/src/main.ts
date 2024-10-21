4963dbfcb4c87e72e81bc1151338dba52d4c84c7
Devin Daniels <devingdaniels@gmail.com>
2024-10-21 10:55:54 +0530

    Add project dependencies.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './common/envs';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'combined.log' }),
+      ],
+    }),
+  });
 
   app.enableCors({
     origin: true,
@@ -14,6 +23,7 @@ async function bootstrap() {
 
   await app.listen(envs.PORT);
 }
+
 bootstrap().then(() => {
   console.log('SUCCESSFULLY STARTED THE SERVER');
 });
